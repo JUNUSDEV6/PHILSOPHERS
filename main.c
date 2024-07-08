9ece4538c92f897978f1403e1fd328d55ea9ea0e
Younes Hanafi <younes-hanafi97@hotmail.com>
2024-07-08 15:55:47 +0200

    1

--- main.c
+++ main.c
@@ -6,17 +6,55 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 12:06:55 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/07/08 14:41:23 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/07/08 15:38:55 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static long	ft_atol(const char *str)
+{
+	long	rlt;
+	char	sign;
+
+	rlt = 0;
+	sign = 1;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == 45 || *str == 43)
+	{
+		if (*str == 45)
+			sign = -1;
+		str++;
+	}
+	while (*str >= 48 && *str <= 57)
+	{
+		rlt = rlt * 10 + *str - 48;
+		str++;
+	}
+	return (rlt * sign);
+}
+
+static void	init_philo(t_data *data)
+{
+
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	*data;
-
+	
 	data = NULL;
-
+	if (argc < 5 || argc > 6)
+		return (printf("Error: Try with 5 or 6 arg"));
+	data.nbr_philo = ft_atol(argv[1]);
+	data.time_to_die = ft_atol(argv[2]);
+	data.time_to_eat = ft_atol(argv[3]);
+	data.time_to_sleep = ft_atol(argv[4]);
+	if (argc == 6)
+		data.nbr_eat = ft_atol(argv[5]);
+	else
+		data.nbr_eat = -1;
+	init_philo(&data);
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,25 +6,34 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 12:14:58 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/07/08 14:43:57 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/07/08 15:41:59 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <pthread.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 typedef	struct	s_philo
 {
 	unsigned int	id;
-	unsigned int	eat_time;
-	unsigned int	die_time;
-	unsigned int	thk_time;
-	unsigned int	sleep_time;
-	unsigned int	right_fork;
-	unsigned int	left_fork;
-	t_data			*data;
-
+	
+	pthread_mutex_t	left_fork;
+	pthread_mutex_t	right_fork;
+	struct s_data	*data;
 }				t_philo;
 
 typedef	struct	s_data
 {
-	
+	unsigned int	nbr_philo;
+	unsigned int	time_to_die;
+	unsigned int	time_to_eat;
+	unsigned int	time_to_sleep;
+	unsigned int	nbr_eat;
+	long long		start_time;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*print_lock;
 	t_philo		*philo;
 }				t_data;
