9513c8b286ac0c7bf95dab77d3eee9783c5f2761
Younes Hanafi <younes-hanafi97@hotmail.com>
2024-07-09 11:11:13 +0200

    1

--- philo.h
+++ philo.h
@@ -6,15 +6,19 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 12:14:58 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/07/08 15:41:59 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/07/09 11:10:52 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef	PHILO_H
+#define	PHILO_H
 
 #include <pthread.h>
 #include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limit.h>
 
 typedef	struct	s_philo
 {
@@ -37,3 +41,4 @@ typedef	struct	s_data
 	pthread_mutex_t	*print_lock;
 	t_philo		*philo;
 }				t_data;
+
